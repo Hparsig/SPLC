f8925d64e0fcfc0fafcf99c1fa8099a70e609403
Michael Jespersen <michael.jespersen@gmail.com>
2013-09-06 12:17:43 +0200

    added notes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Content
 
 This repository contains the weekly exercices due to the course "Programming languages, concepts and implementation" :
 https://blog.itu.dk/SPLC-E2013/
+
+Reference types, Value Types
+============================
+
+Primitive types = Value types (int, char etc.)
+Construct types = Reference type (class, struct, array etc.)
--- week1/exercices/Product.cs
+++ week1/exercices/Product.cs
@@ -2,20 +2,14 @@
  * Date : 30 august 2013
  * Time : 14:00
  */
-
 using System;
-
 namespace SPLC.week1{
-
 	class Product : CompositeExp{
-
 		public Product(IExpression lval, IExpression rval):base(lval, rval){}
 		
 		public override double Eval(){
 			return lval.Eval() * rval.Eval();
 		}
 		public override string ToString(){ return string.Format("({0} * {1})", lval, rval); }
-
 	}
-
 }
